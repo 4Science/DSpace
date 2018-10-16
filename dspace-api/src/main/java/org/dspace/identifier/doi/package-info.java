d918d9d4b7f237ca4af5cf2eab458df2431e56bc
UlrichHahn <43986331+UlrichHahn@users.noreply.github.com>
2018-10-16 13:28:04 +0200

    Update package-info.java

--- dspace-api/src/main/java/org/dspace/identifier/doi/package-info.java
+++ dspace-api/src/main/java/org/dspace/identifier/doi/package-info.java
@@ -4,33 +4,8 @@
  * tree and available online at
  *
  * http://www.dspace.org/license/
- *
- * Make requests to the DOI registration angencies, f.e.to
- * <a href='http://n2t.net/ezid/'>EZID</a> DOI service, and analyze the responses.
- *
- * <p>
- * Use {@link org.dspace.identifier.ezid.EZIDRequestFactory#getInstance} to
- * configure an {@link org.dspace.identifier.ezid.EZIDRequest}
- * with your authority number and credentials.  {@code EZIDRequest} encapsulates
- * EZID's operations (lookup, create/mint, modify, delete...).
- * An operation returns an {@link org.dspace.identifier.ezid.EZIDResponse} which
- * gives easy access to EZID's status code and value, status of the underlying
- * HTTP request, and key/value pairs found in the response body (if any).
- * </p>
- *
- * Make requests to the DOI registration angencies, f.e.to
- * <a href='http://n2t.net/ezid/'>EZID</a> DOI service, and analyze the responses.
- *
- * <p>
- * Use {@link org.dspace.identifier.ezid.EZIDRequestFactory#getInstance} to
- * configure an {@link org.dspace.identifier.ezid.EZIDRequest}
- * with your authority number and credentials.  {@code EZIDRequest} encapsulates
- * EZID's operations (lookup, create/mint, modify, delete...).
- * An operation returns an {@link org.dspace.identifier.ezid.EZIDResponse} which
- * gives easy access to EZID's status code and value, status of the underlying
- * HTTP request, and key/value pairs found in the response body (if any).
- * </p>
  */
+
 /**
  * Make requests to the DOI registration angencies, f.e.to
  *  <a href='http://n2t.net/ezid/'>EZID</a> DOI service, and analyze the responses.
@@ -45,4 +20,5 @@
  * HTTP request, and key/value pairs found in the response body (if any).
  * </p>
  */
+
 package org.dspace.identifier.doi;
