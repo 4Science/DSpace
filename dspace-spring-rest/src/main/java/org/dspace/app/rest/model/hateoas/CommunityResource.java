3eba649314d2010c8557a39524d6050340f15415
UlrichHahn <43986331+UlrichHahn@users.noreply.github.com>
2018-10-16 14:24:33 +0200

    license header issue in dspace-spring-rest

--- dspace-spring-rest/src/main/java/org/dspace/app/rest/model/hateoas/CommunityResource.java
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/model/hateoas/CommunityResource.java
@@ -4,20 +4,8 @@
  * tree and available online at
  *
  * http://www.dspace.org/license/
- *
- * The contents of this file are subject to the license and copyright
- * detailed in the LICENSE and NOTICE files at the root of the source
- * tree and available online at
- *
- * http://www.dspace.org/license/
  */
-/**
- * The contents of this file are subject to the license and copyright
- * detailed in the LICENSE and NOTICE files at the root of the source
- * tree and available online at
- *
- * http://www.dspace.org/license/
- */
+
 package org.dspace.app.rest.model.hateoas;
 
 import org.dspace.app.rest.model.CommunityRest;
