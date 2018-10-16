5cc766621cdda6034ac1d5df2c00855c403772f0
UlrichHahn <43986331+UlrichHahn@users.noreply.github.com>
2018-10-16 12:53:12 +0200

    Update package-info.java

--- dspace-services/src/main/java/org/dspace/utils/package-info.java
+++ dspace-services/src/main/java/org/dspace/utils/package-info.java
@@ -4,17 +4,13 @@
  * tree and available online at
  *
  * http://www.dspace.org/license/
+ */
+/** 
  * Utility classes and starting points.
  * Standalone code, for example, can use the framework by instantiating
  * a DSpace object and using its methods to locate other parts of the
  * framework.
  *
- * @see org.dspace.utils.servlet.DSpaceWebappServletFilter
- */
-/** Utility classes and starting points.
- * Standalone code, for example, can use the framework by instantiating
- * a DSpace object and using its methods to locate other parts of the
- * framework.
  * @see org.dspace.utils.servlet.DSpaceWebappServletFilter
  */
 
