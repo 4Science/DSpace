c379987785a06e9b1d8a5036447797f0649ae92d
UlrichHahn <43986331+UlrichHahn@users.noreply.github.com>
2018-10-16 13:35:53 +0200

    Update package-info.java

--- dspace-api/src/main/java/org/dspace/importer/external/metadatamapping/transform/package-info.java
+++ dspace-api/src/main/java/org/dspace/importer/external/metadatamapping/transform/package-info.java
@@ -4,17 +4,12 @@
  * tree and available online at
  *
  * http://www.dspace.org/license/
- *
- * Transformer classes used by services classes (Such as processing/generating queries, altering results, etc)
- *
- * @author Jonas Van Goolen (jonas at atmire dot com)
- *
- * Transformer classes used by services classes (Such as processing/generating queries, altering results, etc)
- * @author Jonas Van Goolen (jonas at atmire dot com)
  */
 
 /**
  * Transformer classes used by services classes (Such as processing/generating queries, altering results, etc)
+ *
  * @author Jonas Van Goolen (jonas at atmire dot com)
  */
+
 package org.dspace.importer.external.metadatamapping.transform;
