043cf25ab9553f02fc594422b402ddf6ae51cdb1
UlrichHahn <43986331+UlrichHahn@users.noreply.github.com>
2018-10-16 13:33:21 +0200

    Update package-info.java

--- dspace-api/src/main/java/org/dspace/importer/external/exception/package-info.java
+++ dspace-api/src/main/java/org/dspace/importer/external/exception/package-info.java
@@ -4,17 +4,12 @@
  * tree and available online at
  *
  * http://www.dspace.org/license/
- *
- * External importer Exception classes + possible handlers
- *
- * @author Jonas Van Goolen (jonas at atmire dot com)
- *
- * External importer Exception classes + possible handlers
- * @author Jonas Van Goolen (jonas at atmire dot com)
  */
 
 /**
  * External importer Exception classes + possible handlers
+ * 
  * @author Jonas Van Goolen (jonas at atmire dot com)
  */
+
 package org.dspace.importer.external.exception;
