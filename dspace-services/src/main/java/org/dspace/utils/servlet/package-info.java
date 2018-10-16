3d022efcf5acee6e740028d3909a9858f53c594e
UlrichHahn <43986331+UlrichHahn@users.noreply.github.com>
2018-10-16 12:52:15 +0200

    Update package-info.java

--- dspace-services/src/main/java/org/dspace/utils/servlet/package-info.java
+++ dspace-services/src/main/java/org/dspace/utils/servlet/package-info.java
@@ -4,9 +4,6 @@
  * tree and available online at
  *
  * http://www.dspace.org/license/
- *
- * Connect a servlet container with the DSpace Services framework, in a
- * web application context.
  */
 /**
  * Connect a servlet container with the DSpace Services framework, in a
