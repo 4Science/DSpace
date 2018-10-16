1cc5f518accaf906e95b44f4b3da99ca06229545
UlrichHahn <43986331+UlrichHahn@users.noreply.github.com>
2018-10-16 13:26:06 +0200

    Update DiscoverySortConfiguration.java

--- dspace-api/src/main/java/org/dspace/discovery/configuration/DiscoverySortConfiguration.java
+++ dspace-api/src/main/java/org/dspace/discovery/configuration/DiscoverySortConfiguration.java
@@ -4,28 +4,9 @@
  * tree and available online at
  *
  * http://www.dspace.org/license/
- *
- * The contents of this file are subject to the license and copyright
- * detailed in the LICENSE and NOTICE files at the root of the source
- * tree and available online at
- *
- * http://www.dspace.org/license/
- *
- * The contents of this file are subject to the license and copyright
- * detailed in the LICENSE and NOTICE files at the root of the source
- * tree and available online at
- *
- * http://www.dspace.org/license/
  */
-/**
- * The contents of this file are subject to the license and copyright
- * detailed in the LICENSE and NOTICE files at the root of the source
- * tree and available online at
- *
- * http://www.dspace.org/license/
- */
-package org.dspace.discovery.configuration;
 
+package org.dspace.discovery.configuration;
 
 import java.util.ArrayList;
 import java.util.List;
