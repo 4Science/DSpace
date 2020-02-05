36eef5dc1eb7ec1465e7f242d0eb23c7d80d1bb1
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2020-02-05 17:07:11 +0100

    [CST-1984] Improvements to fonds authority to show fonds hierarchy

--- dspace-cris/api/src/main/java/org/dspace/app/cris/integration/FondAuthority.java
+++ dspace-cris/api/src/main/java/org/dspace/app/cris/integration/FondAuthority.java
@@ -1,14 +1,20 @@
 package org.dspace.app.cris.integration;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.dspace.app.cris.model.ACrisObject;
 import org.dspace.app.cris.model.ResearchObject;
 import org.dspace.app.cris.service.ApplicationService;
-import org.dspace.app.cris.util.ResearcherPageUtils;
+import org.dspace.content.Metadatum;
 import org.dspace.utils.DSpace;
 
 public class FondAuthority extends DOAuthority
 {
+	ApplicationService applicationService = new DSpace().getServiceManager().getServiceByName(
+            "applicationService", ApplicationService.class);
 
     /** The logger */
     private static Logger log = Logger.getLogger(FondAuthority.class);
@@ -40,7 +46,36 @@ public class FondAuthority extends DOAuthority
     @Override
     protected String getDisplayEntry(ACrisObject cris)
     {
-        return ResearcherPageUtils.getDisplayEntry(cris);
+        if (cris instanceof ResearchObject)
+        {
+            if (cris instanceof ResearchObject
+                    && cris.getTypeText().equals("crisfonds"))
+            {
+                List<String> results = new ArrayList<>();
 
+                generateTree(cris, results);
+
+                return StringUtils.join(results, " > ");
+            }
+        }
+
+        return cris.getName();
+    }
+
+    private void generateTree(ACrisObject cris, List<String> results)
+    {
+        Metadatum[] fondsparents = cris.getMetadata(
+                cris.getTypeText(), "fondsparent", null, null);
+
+        if (fondsparents != null && fondsparents.length > 0) {
+            for (Metadatum fondsparent : fondsparents) {
+                ResearchObject parent = applicationService.getEntityByCrisId(fondsparent.authority,
+                        ResearchObject.class);
+
+                generateTree(parent, results);
+            }
+        }
+
+        results.add(cris.getName());
     }
 }
