b55fa8fc27a436ce327e587ce6ffce2007b59223
Nikita Krivonosov <nikita.krivonosov@4science.com>
2023-12-06 15:20:18 +0100

    [GLAM-578] MAG: add integrations tests to verify the addition the metadata from manifest file

--- dspace-api/src/test/java/org/dspace/app/packager/MagIngesterIT.java
+++ dspace-api/src/test/java/org/dspace/app/packager/MagIngesterIT.java
@@ -21,6 +21,7 @@ import org.dspace.content.Bitstream;
 import org.dspace.content.Bundle;
 import org.dspace.content.Collection;
 import org.dspace.content.Item;
+import org.dspace.content.MetadataValue;
 import org.dspace.content.factory.ContentServiceFactory;
 import org.dspace.content.service.BitstreamService;
 import org.dspace.content.service.InstallItemService;
@@ -119,6 +120,81 @@ public class MagIngesterIT extends AbstractIntegrationTestWithDatabase {
         String iiifTocValue = bitstreamService
                 .getMetadataFirstValue(tiffBitstreams.get(0), "iiif", "toc", null, Item.ANY);
         assertNull(iiifTocValue);
+    }
+
+    @Test
+    public void testShouldAddMetadataWhenTagExistsInManifest() throws Exception {
+        context.turnOffAuthorisationSystem();
+
+        Collection magCollection = CollectionBuilder
+                .createCollection(context, parentCommunity, "123456789/31")
+                .withName("MagCollection2")
+                .withEntityType("Publication").build();
+
+        String archiveClassPath = "classpath:org/dspace/app/itemimport/UNIBA_MAG_archive_without_stru.zip";
+        Resource archive = new DSpace().getServiceManager().getApplicationContext().getResource(archiveClassPath);
+
+        runDSpaceScript("packager",
+                "-e", admin.getEmail(),
+                "-p", magCollection.getHandle(),
+                "-t", "MAG",
+                archive.getFile().getPath());
+
+        Iterator<Item> itemsIterator = itemService.findAllByCollection(context, magCollection);
+        List<Item> items = IteratorUtils.toList(itemsIterator);
+        assertEquals(1, items.size());
+
+        List<MetadataValue> identifiers = itemService.getMetadata(items.get(0), "dc", "identifier", "other", null);
+        assertEquals(1, identifiers.size());
+        assertEquals("UNIBA_BFB_2069_2_", identifiers.get(0).getValue());
+
+        List<MetadataValue> languages = itemService.getMetadata(items.get(0), "dc", "language", "iso", null);
+        assertEquals(1, languages.size());
+        assertEquals("fr", languages.get(0).getValue());
+
+        List<MetadataValue> dates = itemService.getMetadata(items.get(0), "dc", "date", "issued", null);
+        assertEquals(1, dates.size());
+        assertEquals("1829", dates.get(0).getValue());
+
+        List<MetadataValue> titles = itemService.getMetadata(items.get(0), "dc", "title", null, null);
+        assertEquals(1, titles.size());
+        assertEquals("Histoire naturelle de Pline traduction nouvelle par M. Ajasson de Grandsagne annotée" +
+                " par MM. Beudant, Brongniart, G. Cuvier, et al. Tome quatrième", titles.get(0).getValue());
+    }
+
+    @Test
+    public void testShouldNotAddMetadataWhenTagNotExistsInManifest() throws Exception {
+        context.turnOffAuthorisationSystem();
+
+        Collection magCollection = CollectionBuilder
+                .createCollection(context, parentCommunity, "123456789/31")
+                .withName("MagCollection2")
+                .withEntityType("Publication").build();
+
+        String archiveClassPath = "classpath:org/dspace/app/itemimport/UNIBA_MAG_archive_with_stru.zip";
+        Resource archive = new DSpace().getServiceManager().getApplicationContext().getResource(archiveClassPath);
+
+        runDSpaceScript("packager",
+                "-e", admin.getEmail(),
+                "-p", magCollection.getHandle(),
+                "-t", "MAG",
+                archive.getFile().getPath());
+
+        Iterator<Item> itemsIterator = itemService.findAllByCollection(context, magCollection);
+        List<Item> items = IteratorUtils.toList(itemsIterator);
+        assertEquals(1, items.size());
+
+        List<MetadataValue> identifiers = itemService.getMetadata(items.get(0), "dc", "identifier", "other", null);
+        assertEquals(1, identifiers.size());
+
+        List<MetadataValue> languages = itemService.getMetadata(items.get(0), "dc", "language", "iso", null);
+        assertEquals(0, languages.size());
+
+        List<MetadataValue> dates = itemService.getMetadata(items.get(0), "dc", "date", "issued", null);
+        assertEquals(0, dates.size());
+
+        List<MetadataValue> titles = itemService.getMetadata(items.get(0), "dc", "title", null, null);
+        assertEquals(0, titles.size());
     }
 
 }
