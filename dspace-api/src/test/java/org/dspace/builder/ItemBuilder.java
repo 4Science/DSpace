0d3a376bbc57323cc2db7aa17602d31217e427f8
Mattia Vianelli <mattia.vianelli@4science.com>
2024-08-01 10:32:19 +0200

    CST-14906 Code fix after cherrypick

--- dspace-api/src/test/java/org/dspace/builder/ItemBuilder.java
+++ dspace-api/src/test/java/org/dspace/builder/ItemBuilder.java
@@ -88,6 +88,14 @@ public class ItemBuilder extends AbstractDSpaceObjectBuilder<Item> {
                                 null, authorName, authority, confidence);
     }
 
+    public ItemBuilder withPublisher(String publisher) {
+        return addMetadataValue(item, "dc", "publisher", null, publisher);
+    }
+
+    public ItemBuilder withPatentNo(String patentNo) {
+        return addMetadataValue(item, "dc", "identifier", "patentno", patentNo);
+    }
+
     public ItemBuilder withEditor(final String editorName) {
         return addMetadataValue(item, MetadataSchemaEnum.DC.getName(), "contributor", "editor", editorName);
     }
@@ -366,6 +374,30 @@ public class ItemBuilder extends AbstractDSpaceObjectBuilder<Item> {
 
     public ItemBuilder withOtherIdentifier(String identifier) {
         return addMetadataValue(item, "dc", "identifier", "other", identifier);
+    }
+
+    public ItemBuilder withFundingInvestigator(String investigator) {
+        return addMetadataValue(item, "crisfund", "investigators", null, investigator);
+    }
+
+    public ItemBuilder withFundingInvestigator(String investigator, String authority) {
+        return addMetadataValue(item, "crisfund", "investigators", null, null, investigator, authority, 600);
+    }
+
+    public ItemBuilder withFundingCoInvestigator(String investigator) {
+        return addMetadataValue(item, "crisfund", "coinvestigators", null, investigator);
+    }
+
+    public ItemBuilder withFundingCoInvestigator(String investigator, String authority) {
+        return addMetadataValue(item, "crisfund", "coinvestigators", null, null, investigator, authority, 600);
+    }
+
+    public ItemBuilder withFundingIdentifier(String identifier) {
+        return addMetadataValue(item, "oairecerif", "funding", "identifier", identifier);
+    }
+
+    public ItemBuilder withFundingAwardUrl(String url) {
+        return addMetadataValue(item, "crisfund", "award", "url", url);
     }
 
     /**
