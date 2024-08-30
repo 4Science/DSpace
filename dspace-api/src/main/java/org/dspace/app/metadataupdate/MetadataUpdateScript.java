9682f6320812b6ea978eee596dba0d21365f46dc
eskander <mohamed.eskander@4science.com>
2024-08-30 14:16:01 +0200

    [GLAM-697] new script to replace metdata values
    (cherry picked from commit bf366bdf22da2fac2a8b023837e6e06c7410bf0c)
    
    # Conflicts:
    #	dspace-api/src/test/data/dspaceFolder/config/spring/api/scripts.xml
    #	dspace/config/spring/rest/scripts.xml

---
+++ dspace-api/src/main/java/org/dspace/app/metadataupdate/MetadataUpdateScript.java
@@ -0,0 +1,132 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.metadataupdate;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.UUID;
+
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.lang.StringUtils;
+import org.dspace.authorize.AuthorizeException;
+import org.dspace.content.Item;
+import org.dspace.content.MetadataFieldName;
+import org.dspace.content.factory.ContentServiceFactory;
+import org.dspace.content.service.ItemService;
+import org.dspace.core.Context;
+import org.dspace.discovery.IndexableObject;
+import org.dspace.discovery.SearchService;
+import org.dspace.eperson.EPerson;
+import org.dspace.eperson.factory.EPersonServiceFactory;
+import org.dspace.scripts.DSpaceRunnable;
+import org.dspace.util.SimpleMapConverter;
+import org.dspace.utils.DSpace;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MetadataUpdateScript
+    extends DSpaceRunnable<MetadataUpdateScriptConfiguration<MetadataUpdateScript>> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MetadataUpdateScript.class);
+    private ItemService itemService;
+    private SearchService searchService;
+    private SimpleMapConverter simpleMapConverter;
+    protected Context context;
+    protected MetadataFieldName metadata;
+    protected String entityType;
+
+    @Override
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public MetadataUpdateScriptConfiguration<MetadataUpdateScript> getScriptConfiguration() {
+        MetadataUpdateScriptConfiguration configuration = new DSpace().getServiceManager()
+                .getServiceByName("metadata-update", MetadataUpdateScriptConfiguration.class);
+        return configuration;
+    }
+
+    @Override
+    public void setup() throws ParseException {
+        DSpace dSpace = new DSpace();
+        this.itemService = ContentServiceFactory.getInstance().getItemService();
+        this.searchService = dSpace.getSingletonService(SearchService.class);
+        this.simpleMapConverter = dSpace.getServiceManager()
+                .getServiceByName("mapConverterMetadataValues", SimpleMapConverter.class);
+
+        metadata = new MetadataFieldName(commandLine.getOptionValue("m"));
+        entityType = commandLine.getOptionValue("en");
+    }
+
+    @Override
+    public void internalRun() throws Exception {
+
+        context = new Context(Context.Mode.BATCH_EDIT);
+        assignCurrentUserInContext(context);
+        assignSpecialGroupsInContext(context);
+
+        try {
+            simpleMapConverter.getMapping().forEach((key, value) -> {
+
+                if (!value.isEmpty() && !value.equals(key)) {
+
+                    List<IndexableObject> indexableObjects = findIndexableObjects(key);
+
+                    indexableObjects.forEach(indexableObject -> {
+                        Item item = ((Item) indexableObject.getIndexedObject());
+                        try {
+                            String currentValue = itemService.getMetadataFirstValue(item,
+                                    metadata.schema, metadata.element, metadata.qualifier, Item.ANY);
+                            if (StringUtils.equals(currentValue, key)) {
+                                itemService.setMetadataSingleValue(context, item, metadata, Item.ANY, value);
+                                itemService.update(context, item);
+                            }
+                        } catch (SQLException | AuthorizeException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+                }
+
+            });
+
+            context.complete();
+            handler.logInfo("Metadata " + metadata + " has been updated successfully");
+        } catch (Exception e) {
+            handler.handleException(e);
+            LOGGER.error(e.getMessage(), e);
+            context.abort();
+        } finally {
+            context.restoreAuthSystemState();
+        }
+    }
+
+    private List<IndexableObject> findIndexableObjects(String value) {
+        return searchService.search(context, "*:*",
+                "lastModified", false, 0, Integer.MAX_VALUE,
+                "search.resourcetype:Item",
+                "dspace.entity.type:" + entityType,
+                metadata.toString() + ":\"" + searchService.escapeQueryChars(value) + "\"");
+    }
+
+    protected void assignCurrentUserInContext(Context context) throws ParseException {
+        UUID uuid = getEpersonIdentifier();
+        if (uuid != null) {
+            EPerson ePerson;
+            try {
+                ePerson = EPersonServiceFactory.getInstance().getEPersonService().find(context, uuid);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            context.setCurrentUser(ePerson);
+        }
+    }
+
+    private void assignSpecialGroupsInContext(Context context) {
+        for (UUID uuid : handler.getSpecialGroups()) {
+            context.setSpecialGroup(uuid);
+        }
+    }
+
+}
---
+++ dspace-api/src/test/java/org/dspace/app/metadataupdate/MetadataUpdateScriptIT.java
@@ -0,0 +1,132 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.metadataupdate;
+
+import static org.dspace.app.matcher.MetadataValueMatcher.with;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasItem;
+
+import org.dspace.AbstractIntegrationTestWithDatabase;
+import org.dspace.app.launcher.ScriptLauncher;
+import org.dspace.app.scripts.handler.impl.TestDSpaceRunnableHandler;
+import org.dspace.builder.CollectionBuilder;
+import org.dspace.builder.CommunityBuilder;
+import org.dspace.builder.ItemBuilder;
+import org.dspace.content.Collection;
+import org.dspace.content.Item;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MetadataUpdateScriptIT extends AbstractIntegrationTestWithDatabase {
+
+    private Collection collection;
+
+    @Before
+    public void setup() {
+        context.turnOffAuthorisationSystem();
+
+        parentCommunity = CommunityBuilder.createCommunity(context)
+            .withName("Parent Community")
+            .build();
+
+        collection = CollectionBuilder.createCollection(context, parentCommunity)
+                .withName("Collection")
+                .withEntityType("Publication")
+                .build();
+        context.restoreAuthSystemState();
+
+    }
+
+    @Test
+    public void testUpdatingMetadata() throws Exception {
+
+        context.turnOffAuthorisationSystem();
+
+        Item itemOne = createItem("Test publication one", "english");
+
+        Item itemTwo = createItem("Test publication two", "English");
+
+        Item itemThree = createItem("Test publication three", "ENGLISH");
+
+        Item itemFour = createItem("Test publication four", "eng");
+
+        Item itemFive = createItem("Test publication five", "ita");
+
+        Item itemSix = createItem("Test publication six", "fra");
+
+        Item itemSeven = createItem("Test publication seven", "English:(French and German)");
+
+        Item itemEight = createItem("Test publication seven", "English:(French and German and Russian)");
+
+        context.restoreAuthSystemState();
+
+        TestDSpaceRunnableHandler runnableHandler = runScript("Publication");
+
+        assertThat(runnableHandler.getErrorMessages(), empty());
+        assertThat(runnableHandler.getInfoMessages(),
+                contains("Metadata dc.language.iso has been updated successfully"));
+
+        itemOne = context.reloadEntity(itemOne);
+        itemTwo = context.reloadEntity(itemTwo);
+        itemThree = context.reloadEntity(itemThree);
+        itemFour = context.reloadEntity(itemFour);
+        itemFive = context.reloadEntity(itemFive);
+        itemSix = context.reloadEntity(itemSix);
+        itemSeven = context.reloadEntity(itemSeven);
+        itemEight = context.reloadEntity(itemEight);
+
+        assertThat(itemOne.getMetadata(), hasItem(with("dc.language.iso", "en", "2", 0, 500)));
+        assertThat(itemTwo.getMetadata(), hasItem(with("dc.language.iso", "en", "2", 0, 500)));
+        assertThat(itemThree.getMetadata(), hasItem(with("dc.language.iso", "en", "2", 0, 500)));
+        assertThat(itemFour.getMetadata(), hasItem(with("dc.language.iso", "en", "2", 0, 500)));
+        assertThat(itemFive.getMetadata(), hasItem(with("dc.language.iso", "it", "6", 0, 500)));
+        assertThat(itemSix.getMetadata(), hasItem(with("dc.language.iso", "fr", "5", 0, 500)));
+        assertThat(itemSeven.getMetadata(), hasItem(with("dc.language.iso", "en | fr | de", null, 0, 300)));
+        assertThat(itemEight.getMetadata(), hasItem(with("dc.language.iso", "en | fr | de | ru", null, 0, 300)));
+    }
+
+    @Test
+    public void testUpdatingMetadataNotExistingMappingKeys() throws Exception {
+
+        context.turnOffAuthorisationSystem();
+        Item item = ItemBuilder.createItem(context, collection)
+                .withTitle("Test publication one")
+                .withLanguage("fake")
+                .build();
+        context.restoreAuthSystemState();
+
+        TestDSpaceRunnableHandler runnableHandler = runScript("Publication");
+
+        assertThat(runnableHandler.getErrorMessages(), empty());
+        assertThat(runnableHandler.getInfoMessages(),
+                contains("Metadata dc.language.iso has been updated successfully"));
+
+        item = context.reloadEntity(item);
+
+        assertThat(item.getMetadata(), hasItem(with("dc.language.iso", "fake")));
+    }
+
+    private TestDSpaceRunnableHandler runScript(String entityType)
+            throws InstantiationException, IllegalAccessException {
+        TestDSpaceRunnableHandler runnableHandler = new TestDSpaceRunnableHandler();
+        String[] args = new String[] { "metadata-update", "-m", "dc.language.iso",
+                "-en", entityType, "-e", admin.getEmail()};
+        ScriptLauncher.handleScript(args, ScriptLauncher.getConfig(kernelImpl), runnableHandler, kernelImpl);
+        return runnableHandler;
+    }
+
+    private Item createItem(String title, String language) {
+        return ItemBuilder.createItem(context, collection)
+                .withTitle(title)
+                .withLanguage(language)
+                .build();
+    }
+
+}
