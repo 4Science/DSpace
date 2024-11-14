a7a1806924876e481455a3e05fdf774a842d0272
vincenzo lalomia <vincenzo.lalomia@4science.com>
2024-11-14 11:07:33 +0100

    [UXP-204] moved test resources

--- dspace-api/src/test/java/org/dspace/unpaywall/service/impl/MockUnpaywallClientAPIImpl.java
+++ dspace-api/src/test/java/org/dspace/unpaywall/service/impl/MockUnpaywallClientAPIImpl.java
@@ -8,22 +8,35 @@
 package org.dspace.unpaywall.service.impl;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Optional;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.dspace.services.ConfigurationService;
 import org.dspace.unpaywall.service.UnpaywallClientAPI;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class MockUnpaywallClientAPIImpl implements UnpaywallClientAPI {
-    private static final String BASE_UNPAYWALL_DIR_PATH = "org/dspace/app/unpaywall/";
+    private static final String BASE_UNPAYWALL_DIR_PATH = "assetstore/unpaywall";
 
+    @Autowired
+    private ConfigurationService configurationService;
 
     @Override
     public File downloadResource(String pdfUrl) throws IOException {
-        String responseFileName = BASE_UNPAYWALL_DIR_PATH.concat("unpaywall-api-resource.pdf");
-        return new File(getClass().getClassLoader().getResource(responseFileName).getFile());
+        String filePath = getFilePath("unpaywall-api-resource.pdf");
+
+        return new File(filePath);
+    }
+
+    private String getFilePath(String filename) {
+        return String.format("%s/%s/%s",
+                             configurationService.getProperty("dspace.dir"),
+                             BASE_UNPAYWALL_DIR_PATH ,
+                             filename);
     }
 
     @Override
@@ -31,9 +44,8 @@ public class MockUnpaywallClientAPIImpl implements UnpaywallClientAPI {
         if (StringUtils.endsWith(doi, "/not-found")) {
             return Optional.empty();
         } else {
-            String responseFileName = BASE_UNPAYWALL_DIR_PATH.concat("unpaywall-api-response.json");
-            try (InputStream unpaywallResponseStream = getClass().getClassLoader()
-                        .getResourceAsStream(responseFileName)) {
+            String responseFileName = getFilePath("unpaywall-api-response.json");
+            try (InputStream unpaywallResponseStream = new FileInputStream(responseFileName)) {
                 String unpaywallResponse = IOUtils.toString(unpaywallResponseStream, "UTF-8");
                 return Optional.of(unpaywallResponse);
             } catch (IOException e) {
