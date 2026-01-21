b6fe8170cbf53d0a1b755bf949148c2704e0c5f4
AdamF42 <adamo.fapohunda@4science.com>
2026-01-21 23:48:48 +0100

    [DURACOM-427] Fix ItemRestRepositoryIT to handle allLanguages projection

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/ItemRestRepositoryIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/ItemRestRepositoryIT.java
@@ -3080,7 +3080,8 @@ public class ItemRestRepositoryIT extends AbstractControllerIntegrationTest {
 
         String token = getAuthToken(eperson.getEmail(), password);
 
-        getClient(token).perform(get("/api/core/items/" + item.getID()))
+        getClient(token).perform(get("/api/core/items/" + item.getID())
+                                     .param("projection", "allLanguages"))
             .andExpect(status().isOk())
             .andExpect(jsonPath("$", ItemMatcher.matchItemProperties(item)))
             .andExpect(jsonPath("$.metadata", matchMetadata("dc.title", "Public item 1")))
