b87ea2b4ccf971da870d8858f769b07f367687fc
AdamF42 <adamo.fapohunda@4science.com>
2026-01-22 20:50:29 +0100

    [DURACOM-427] Removed unused code

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/ExternalSourcesRestControllerIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/ExternalSourcesRestControllerIT.java
@@ -49,37 +49,44 @@ public class ExternalSourcesRestControllerIT extends AbstractControllerIntegrati
 
     @Test
     public void findAllExternalSources() throws Exception {
-        getClient().perform(get("/api/integration/externalsources").param("size", "22"))
-                   .andExpect(status().isOk())
-                   .andExpect(jsonPath("$._embedded.externalsources", Matchers.hasItems(
-                       ExternalSourceMatcher.matchExternalSource("mock", "mock", false),
-                       ExternalSourceMatcher.matchExternalSource("orcid", "orcid", false),
-                       ExternalSourceMatcher.matchExternalSource("suggestion", "suggestion", false),
-                       ExternalSourceMatcher.matchExternalSource("scopus", "scopus", false),
-                       ExternalSourceMatcher.matchExternalSource("sherpaJournalIssn", "sherpaJournalIssn", false),
-                       ExternalSourceMatcher.matchExternalSource("sherpaJournal", "sherpaJournal", false),
-                       ExternalSourceMatcher.matchExternalSource("sherpaPublisher", "sherpaPublisher", false),
-                       ExternalSourceMatcher.matchExternalSource("pubmed", "pubmed", false),
-                       ExternalSourceMatcher.matchExternalSource("openaireFunding", "openaireFunding", false),
-                       ExternalSourceMatcher.matchExternalSource("openalexPublication", "openalexPublication", false),
-                       ExternalSourceMatcher.matchExternalSource("openalexPublicationByAuthorId",
-                                                                 "openalexPublicationByAuthorId", false),
-                       ExternalSourceMatcher.matchExternalSource("openalexPublicationByDOI", "openalexPublicationByDOI",
-                                                                 false),
-                       ExternalSourceMatcher.matchExternalSource("openalexPerson", "openalexPerson", false),
-                       ExternalSourceMatcher.matchExternalSource("openalexInstitution", "openalexInstitution", false),
-                       ExternalSourceMatcher.matchExternalSource("openalexPublisher", "openalexPublisher", false),
-                       ExternalSourceMatcher.matchExternalSource("openalexJournal", "openalexJournal", false),
-                       ExternalSourceMatcher.matchExternalSource("openalexFunder", "openalexFunder", false),
-                       ExternalSourceMatcher.matchExternalSource("authorAuthority", "authorAuthority", false),
-                       ExternalSourceMatcher.matchExternalSource("orcidWorks", "orcidWorks", false),
-                       ExternalSourceMatcher.matchExternalSource("datacite", "datacite", false),
-                       ExternalSourceMatcher.matchExternalSource("dataciteProject", "dataciteProject", false),
-                       ExternalSourceMatcher.matchExternalSource("openaireProject", "openaireProject", false)
-                   )))
-                   .andExpect(jsonPath("$.page.totalElements", Matchers.is(22)));
+        getClient().perform(get("/api/integration/externalsources").param("size", "21"))
+                            .andExpect(status().isOk())
+                            .andExpect(jsonPath("$._embedded.externalsources", Matchers.hasItems(
+                                ExternalSourceMatcher.matchExternalSource("mock", "mock", false),
+                                ExternalSourceMatcher.matchExternalSource("orcid", "orcid", false),
+                                ExternalSourceMatcher.matchExternalSource("suggestion", "suggestion", false),
+                                ExternalSourceMatcher.matchExternalSource("scopus", "scopus", false),
+                                ExternalSourceMatcher.matchExternalSource(
+                                    "sherpaJournalIssn", "sherpaJournalIssn", false),
+                                ExternalSourceMatcher.matchExternalSource(
+                                    "sherpaJournal", "sherpaJournal", false),
+                                ExternalSourceMatcher.matchExternalSource(
+                                    "sherpaPublisher", "sherpaPublisher", false),
+                                ExternalSourceMatcher.matchExternalSource(
+                                        "pubmed", "pubmed", false),
+                                ExternalSourceMatcher.matchExternalSource(
+                                    "openaireFunding", "openaireFunding", false),
+                                ExternalSourceMatcher.matchExternalSource(
+                                    "openalexPublication", "openalexPublication", false),
+                                ExternalSourceMatcher.matchExternalSource(
+                                    "openalexPublicationByAuthorId", "openalexPublicationByAuthorId", false),
+                                ExternalSourceMatcher.matchExternalSource(
+                                    "openalexPublicationByDOI", "openalexPublicationByDOI", false),
+                                ExternalSourceMatcher.matchExternalSource(
+                                    "openalexPerson", "openalexPerson", false),
+                                ExternalSourceMatcher.matchExternalSource(
+                                    "openalexInstitution", "openalexInstitution", false),
+                                ExternalSourceMatcher.matchExternalSource(
+                                    "openalexPublisher", "openalexPublisher", false),
+                                ExternalSourceMatcher.matchExternalSource(
+                                    "openalexFunder", "openalexFunder", false),
+                                ExternalSourceMatcher.matchExternalSource(
+                                    "openalexJournal", "openalexJournal", false),
+                                ExternalSourceMatcher.matchExternalSource(
+                                    "authorAuthority", "authorAuthority", false)
+                                )))
+                   .andExpect(jsonPath("$.page.totalElements", Matchers.is(21)));
     }
-
 
     @Test
     public void findOneExternalSourcesExistingSources() throws Exception {
