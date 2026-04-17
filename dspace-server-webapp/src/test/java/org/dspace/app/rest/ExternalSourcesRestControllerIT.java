37b2a4a0cc1ca9dd8d1f0f753aab2235ed0de1e1
Mykhaylo Boychuk <mykhaylo.boychuk@4science.com>
2026-04-18 00:42:58 +0200

    [DSC-2782] fix test

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/ExternalSourcesRestControllerIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/ExternalSourcesRestControllerIT.java
@@ -70,20 +70,9 @@ public class ExternalSourcesRestControllerIT extends AbstractControllerIntegrati
                         ExternalSourceMatcher.matchExternalSource("suggestion", "suggestion", false),
                         ExternalSourceMatcher.matchExternalSource("openaireProject", "openaireProject", false),
                         ExternalSourceMatcher.matchExternalSource("crossref", "crossref", false),
-                        ExternalSourceMatcher.matchExternalSource("orcidWorks", "orcidWorks", false),
-                        ExternalSourceMatcher.matchExternalSource("dataciteProject", "dataciteProject", false),
-                        ExternalSourceMatcher.matchExternalSource("openalexPublication", "openalexPublication", false),
-                        ExternalSourceMatcher.matchExternalSource(
-                                        "openalexPublicationByAuthorId", "openalexPublicationByAuthorId", false),
-                        ExternalSourceMatcher
-                                .matchExternalSource("openalexPublicationByDOI", "openalexPublicationByDOI", false),
-                        ExternalSourceMatcher.matchExternalSource("openalexPerson", "openalexPerson", false),
-                        ExternalSourceMatcher.matchExternalSource("openalexJournal", "openalexJournal", false),
-                        ExternalSourceMatcher.matchExternalSource("openalexInstitution", "openalexInstitution", false),
-                        ExternalSourceMatcher.matchExternalSource("openalexPublisher", "openalexPublisher", false),
-                        ExternalSourceMatcher.matchExternalSource("openalexFunder", "openalexFunder", false)
+                        ExternalSourceMatcher.matchExternalSource("orcidWorks", "orcidWorks", false)
                    )))
-                   .andExpect(jsonPath("$.page.totalElements", Matchers.is(25)));
+                   .andExpect(jsonPath("$.page.totalElements", Matchers.is(16)));
     }
 
     @Test
