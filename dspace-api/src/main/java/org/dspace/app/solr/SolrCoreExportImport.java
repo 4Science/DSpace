68d30fa48a8f9fab5ef2959370895cdd1c716449
Vincenzo Mecca <vincenzo.mecca@4science.com>
2026-06-11 09:54:32 +0200

    refactor(solr): remove unused InputStream import from SolrCoreExportImport
    ref: DSC-2877

--- dspace-api/src/main/java/org/dspace/app/solr/SolrCoreExportImport.java
+++ dspace-api/src/main/java/org/dspace/app/solr/SolrCoreExportImport.java
@@ -11,7 +11,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.math.BigInteger;
 import java.net.URI;
 import java.net.URLEncoder;
